63a1b3647c976544d4adbaa43035c83250c9dddf
Dineshkumar0110 <114792112+Dineshkumar0110@users.noreply.github.com>
2023-01-19 13:22:03 +0530

    Update index.js

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -48,7 +48,6 @@ const Card = ({ name, text, image }) => {
 
   return (
     <CARD>
-      <Image img={Avatar} width="400" height="400" />
       <TEXT>{text}</TEXT>
       <NAME>{name}</NAME>
     </CARD>
